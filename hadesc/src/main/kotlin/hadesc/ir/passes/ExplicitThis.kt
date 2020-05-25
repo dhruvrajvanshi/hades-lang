52147f3b7d0c30eb815011bd1b4ff5be80a3889c
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2020-05-26 04:43:48 +0530

    Refactoring: Factor TransformationPass interface to reduce boilerplate

--- hadesc/src/main/kotlin/hadesc/ir/passes/ExplicitThis.kt
+++ hadesc/src/main/kotlin/hadesc/ir/passes/ExplicitThis.kt
@@ -1,6 +1,5 @@
 package hadesc.ir.passes
 
-import hadesc.assertions.requireUnreachable
 import hadesc.context.Context
 import hadesc.ir.*
 import hadesc.types.Type
@@ -8,174 +7,25 @@ import hadesc.types.Type
 @OptIn(ExperimentalStdlibApi::class)
 class ExplicitThis(
     private val ctx: Context,
-    private val oldModule: IRModule
-) {
-    private val builder = IRBuilder()
-    private val module = IRModule()
-    fun run(): IRModule {
-        for (definition in oldModule) {
-            lowerDefinition(definition)
-        }
-        return module
-    }
+    override val inputModule: IRModule
+): TransformationPass {
+    override val builder = IRBuilder()
+    override val module = IRModule()
 
-    private fun lowerDefinition(definition: IRDefinition): Unit = when (definition) {
-        is IRFunctionDef -> lowerFunctionDef(definition)
-        is IRInterfaceDef -> lowerInterfaceDef(definition)
-        is IRImplementationDef -> lowerImplementationDef(definition)
-        is IRConstDef -> lowerIRConstDef(definition)
-        is IRStructDef -> lowerIRStructDef(definition)
-        is IRExternFunctionDef -> lowerExternFunctionDef(definition)
-    }
-
-    private fun lowerType(type: Type): Type = when (type) {
-        Type.Error -> requireUnreachable()
-        Type.Byte,
-        Type.Void,
-        Type.Bool,
-        Type.CInt,
-        Type.Size -> type
-        is Type.RawPtr -> Type.RawPtr(lowerType(type.to))
-        is Type.Function -> {
-            val nonReceiverParams = type.from.map { lowerType(it) }
-            val from =  if (type.receiver != null)
-                listOf(lowerType(type.receiver)) + nonReceiverParams
-            else nonReceiverParams
-            Type.Function(
-                    receiver = null,
-                    typeParams = type.typeParams,
-                    from = from,
-                    to = lowerType(type.to)
-            )
-        }
-        is Type.Struct -> {
-            Type.Struct(
-                    constructor = lowerType(type.constructor) as Type.Constructor,
-                    memberTypes = type.memberTypes.mapValues { lowerType(it.value) }
-            )
-        }
-        is Type.Constructor -> Type.Constructor(
-                type.binder,
-                name = type.name,
-                params = type.params
-        )
-        is Type.ParamRef -> type
-        is Type.GenericInstance -> requireUnreachable()
-        is Type.Application -> Type.Application(
-                callee = lowerType(type.callee),
-                args = type.args.map { lowerType(it) }
-        )
-        is Type.ThisRef -> requireUnreachable()
-    }
-
-    private fun lowerIRStructDef(definition: IRStructDef) {
-        module.addStructDef(
-                constructorType = lowerType(definition.constructorType) as Type.Function,
-                typeParams = definition.typeParams?.map { lowerTypeParam(it) },
-                name = lowerGlobalName(definition.globalName),
-                instanceType = lowerType(definition.instanceType),
-                fields = definition.fields.mapValues { lowerType(it.value) }
+    override fun lowerFunctionType(type: Type.Function): Type {
+        val nonReceiverParams = type.from.map { lowerType(it) }
+        val from =  if (type.receiver != null)
+            listOf(lowerType(type.receiver)) + nonReceiverParams
+        else nonReceiverParams
+        return Type.Function(
+                receiver = null,
+                typeParams = type.typeParams,
+                from = from,
+                to = lowerType(type.to)
         )
     }
 
-    private fun lowerGlobalName(name: IRGlobalName): IRGlobalName {
-        return name
-    }
-    private fun lowerLocalName(name: IRLocalName): IRLocalName {
-        return name
-    }
-
-    private fun lowerName(name: IRName): IRName = when(name) {
-        is IRLocalName -> lowerLocalName(name)
-        is IRGlobalName -> lowerGlobalName(name)
-    }
-
-    private fun lowerTypeParam(typeParam: IRTypeParam): IRTypeParam {
-        return IRTypeParam(
-                lowerLocalName(typeParam.name),
-                binderLocation = typeParam.binderLocation
-        )
-    }
-
-    private fun lowerExternFunctionDef(definition: IRExternFunctionDef) {
-        module.addExternFunctionDef(
-                name = lowerGlobalName(definition.name),
-                type = lowerType(definition.type) as Type.Function,
-                externName = definition.externName
-        )
-    }
-
-    private fun lowerIRConstDef(definition: IRConstDef) {
-        module.addConstDef(
-                type = lowerType(definition.type),
-                name = lowerGlobalName(definition.name),
-                initializer = lowerValue(definition.initializer)
-        )
-    }
-
-    private fun lowerValue(value: IRValue): IRValue = when(value) {
-        is IRBool -> value
-        is IRByteString -> value
-        is IRCIntConstant -> value
-        is IRVariable -> lowerVariable(value)
-        is IRGetStructField -> lowerGetStructField(value)
-        is IRNullPtr -> lowerNullPtr(value)
-        is IRSizeOf -> lowerSizeOf(value)
-        is IRPointerCast -> lowerPointerCast(value)
-        is IRMethodRef -> requireUnreachable()
-    }
-
-    private fun lowerPointerCast(value: IRPointerCast): IRValue {
-        return IRPointerCast(
-                type = lowerType(value.type),
-                location = value.location,
-                arg = lowerValue(value.arg),
-                toPointerOfType = lowerType(value.toPointerOfType)
-        )
-    }
-
-    private fun lowerSizeOf(value: IRSizeOf): IRValue {
-        return IRSizeOf(
-                type = lowerType(value.type),
-                location = value.location,
-                ofType = lowerType(value.ofType)
-        )
-    }
-
-    private fun lowerNullPtr(value: IRNullPtr): IRValue {
-        return IRNullPtr(
-                location = value.location,
-                type = lowerType(value.type)
-        )
-    }
-
-    private fun lowerGetStructField(value: IRGetStructField): IRValue {
-        return IRGetStructField(
-                type = lowerType(value.type),
-                location = value.location,
-                index = value.index,
-                rhs = value.rhs,
-                lhs = lowerValue(value.lhs)
-        )
-    }
-
-    private fun lowerVariable(variable: IRVariable): IRValue {
-        return IRVariable(
-                type = lowerType(variable.type),
-                location = variable.location,
-                name = lowerName(variable.name)
-        )
-    }
-
-    private fun lowerImplementationDef(definition: IRImplementationDef) {
-        TODO()
-    }
-
-    private fun lowerInterfaceDef(definition: IRInterfaceDef) {
-        TODO()
-    }
-
-    private fun lowerFunctionDef(definition: IRFunctionDef) {
+    override fun lowerFunctionDef(definition: IRFunctionDef) {
         val newEntryBlock = IRBlock()
         lowerBlock(definition.entryBlock, newEntryBlock)
         val name = lowerGlobalName(definition.name)
@@ -206,66 +56,7 @@ class ExplicitThis(
 
     }
 
-    private fun lowerParam(it: IRParam, offsetBy: Int = 0): IRParam {
-        return IRParam(
-                lowerLocalName(it.name),
-                type = lowerType(it.type),
-                location = it.location,
-                functionName = lowerGlobalName(it.functionName),
-                index = it.index + offsetBy
-        )
-    }
-
-    private fun lowerBlock(oldBlock: IRBlock, newBlock: IRBlock) {
-        builder.position = newBlock
-        for (instruction in oldBlock) {
-            lowerInstruction(instruction)
-        }
-    }
-
-    private fun lowerInstruction(instruction: IRInstruction): Unit = when(instruction) {
-        is IRReturnInstruction -> lowerReturnInstruction(instruction)
-        is IRAlloca -> lowerAllocaInstruction(instruction)
-        is IRStore -> lowerStoreInstruction(instruction)
-        is IRLoad -> lowerLoadInstruction(instruction)
-        IRReturnVoidInstruction -> lowerRetVoidInstruction(instruction)
-        is IRBinOp -> lowerBinOpInstruction(instruction)
-        is IRCall -> lowerCallInstruction(instruction)
-        is IRNot -> lowerNotInstruction(instruction)
-        is IRBr -> lowerBrInstruction(instruction)
-        is IRJump -> lowerJumpInstruction(instruction)
-    }
-
-    private fun lowerRetVoidInstruction(instruction: IRInstruction) {
-        builder.buildRetVoid()
-    }
-
-    private fun lowerJumpInstruction(instruction: IRJump) {
-        builder.buildJump(
-                instruction.location,
-                instruction.label
-        )
-    }
-
-    private fun lowerBrInstruction(instruction: IRBr) {
-        builder.buildBranch(
-                instruction.location,
-                condition = lowerValue(instruction.condition),
-                ifTrue = instruction.ifTrue,
-                ifFalse = instruction.ifFalse
-        )
-    }
-
-    private fun lowerNotInstruction(instruction: IRNot) {
-        builder.buildNot(
-                type = lowerType(instruction.type),
-                location = instruction.location,
-                name = lowerLocalName(instruction.name),
-                value = lowerValue(instruction.arg)
-        )
-    }
-
-    private fun lowerCallInstruction(instruction: IRCall) {
+    override fun lowerCallInstruction(instruction: IRCall) {
         val callee = if (instruction.callee is IRMethodRef)
             IRVariable(
                     location = instruction.callee.location,
@@ -285,44 +76,6 @@ class ExplicitThis(
                 typeArgs = instruction.typeArgs?.map { lowerType(it) },
                 callee = callee,
                 args = args
-        )
-    }
-
-    private fun lowerBinOpInstruction(instruction: IRBinOp) {
-        builder.buildBinOp(
-                type = lowerType(instruction.type),
-                name = lowerLocalName(instruction.name),
-                lhs = lowerValue(instruction.lhs),
-                operator = instruction.operator,
-                rhs = lowerValue(instruction.rhs)
-        )
-    }
-
-    private fun lowerLoadInstruction(instruction: IRLoad) {
-        builder.buildLoad(
-                type = lowerType(instruction.type),
-                name = lowerLocalName(instruction.name),
-                ptr = lowerValue(instruction.ptr)
-        )
-    }
-
-    private fun lowerStoreInstruction(instruction: IRStore) {
-        builder.buildStore(
-                ptr = lowerValue(instruction.ptr),
-                value = lowerValue(instruction.value)
-        )
-    }
-
-    private fun lowerAllocaInstruction(instruction: IRAlloca) {
-        builder.buildAlloca(
-                type = lowerType(instruction.type),
-                name = lowerLocalName(instruction.name)
-        )
-    }
-
-    private fun lowerReturnInstruction(instruction: IRReturnInstruction) {
-        builder.buildReturn(
-                lowerValue(instruction.value)
         )
     }
 }
---
+++ hadesc/src/main/kotlin/hadesc/ir/passes/TypeTransformer.kt
@@ -0,0 +1,62 @@
+package hadesc.ir.passes
+
+import hadesc.assertions.requireUnreachable
+import hadesc.types.Type
+
+interface TypeTransformer {
+    fun lowerType(type: Type): Type = when (type) {
+        Type.Error -> requireUnreachable()
+        Type.Byte -> lowerByteType(type)
+        Type.Void -> lowerVoidType(type)
+        Type.Bool -> lowerBoolType(type)
+        Type.CInt -> lowerCIntType(type)
+        Type.Size -> lowerSizeType(type)
+        is Type.RawPtr -> lowerRawPtrType(type)
+        is Type.Function -> lowerFunctionType(type)
+        is Type.Struct -> lowerStructType(type)
+        is Type.Constructor -> lowerTypeConstructor(type)
+        is Type.ParamRef -> lowerParamRefType(type)
+        is Type.GenericInstance -> requireUnreachable()
+        is Type.Application -> lowerTypeApplication(type)
+        is Type.ThisRef -> lowerThisRefType(type)
+    }
+
+    fun lowerFunctionType(type: Type.Function): Type = Type.Function(
+            receiver = type.receiver?.let { lowerType(it) },
+            typeParams = type.typeParams,
+            from = type.from.map { lowerType(it) },
+            to = lowerType(type.to)
+    )
+
+    fun lowerSizeType(type: Type): Type = type
+
+    fun lowerCIntType(type: Type): Type = type
+
+    fun lowerBoolType(type: Type): Type = type
+
+    fun lowerStructType(type: Type.Struct): Type = Type.Struct(
+            constructor = lowerType(type.constructor) as Type.Constructor,
+            memberTypes = type.memberTypes.mapValues { lowerType(it.value) }
+    )
+
+    fun lowerTypeConstructor(type: Type.Constructor): Type = Type.Constructor(
+            type.binder,
+            name = type.name,
+            params = type.params
+    )
+
+    fun lowerParamRefType(type: Type.ParamRef): Type = type
+
+    fun lowerTypeApplication(type: Type.Application): Type = Type.Application(
+            callee = lowerType(type.callee),
+            args = type.args.map { lowerType(it) }
+    )
+
+    fun lowerThisRefType(type: Type.ThisRef): Type = type
+
+    fun lowerVoidType(type: Type): Type = type
+
+    fun lowerRawPtrType(type: Type.RawPtr): Type = Type.RawPtr(lowerType(type.to))
+
+    fun lowerByteType(type: Type): Type = type
+}
