f581f28ff348e45c28e2bb95e8e404ee7d32d2e6
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2020-06-07 10:42:55 +0530

    add __hds_pointer_add to runtime.c

--- runtime.c
+++ runtime.c
@@ -1,7 +1,13 @@
+#include <stdlib.h>
+
 extern void hades_main();
 
 int main() {
     hades_main();
     return 0;
+}
+
+void* __hds_pointer_add(void* ptr, size_t size) {
+    return ptr + size;
 }
 
