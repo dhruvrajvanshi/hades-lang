b2445bd62f17ad8e8b9f62c6d2b4450ab6d69ae6
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2020-06-15 10:32:13 +0530

    Add String type for self hosted lib

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+set -e
+
+./gradlew :hadesboot:installDist
+
+HADESBOOT="./hadesboot/build/install/hadesboot/bin/hadesboot"
+
+HDFLAGS="--runtime runtime.c --directories stdlib src"
+
+TEST_FILES=(
+  src/lib/string_test
+)
+DIRECTORIES=(
+  src/lib
+)
+rm -rf test_build
+
+for directory in $DIRECTORIES
+do
+  mkdir -p test_build/$directory
+done
+
+for file in $TEST_FILES
+do
+  $HADESBOOT $HDFLAGS --main $file.hds --output test_build/$file --cflags "-D DEBUG"
+  ./test_build/$file
+done
+
