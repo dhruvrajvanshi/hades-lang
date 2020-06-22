db125a3b57f85a3ae1df9d5154aa6e51f613b646
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2020-06-22 21:12:22 +0530

    Add c sources for libc bindings

---
+++ stdlib/libc.c
@@ -0,0 +1,17 @@
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+const char* hdc_dirent_name(struct dirent* entry) {
+    return entry->d_name;
+}
+
+unsigned char hdc_dirent_type(struct dirent* entry)  {
+    return entry->d_type;
+}
+
+unsigned char hdc_dirent_is_directory(struct dirent* entry) {
+    return S_ISDIR(entry->d_type);
+}
+
