0e1fa4d28fc1e72157d6294c1e5f590823238c9c
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2020-06-26 02:00:52 +0530

    Update extension to listen to change events for hds files

---
+++ ide/vscode-hades/client/extension.js
@@ -0,0 +1,34 @@
+"use strict";
+exports.__esModule = true;
+exports.deactivate = exports.activate = void 0;
+var vscode_1 = require("vscode");
+var vscode_languageclient_1 = require("vscode-languageclient");
+var client;
+function activate(context) {
+    // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise the run options are used
+    var serverOptions = {
+        command: "./hades-server"
+    };
+    // Options to control the language client
+    var clientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [{ scheme: 'file', language: 'hades' }],
+        synchronize: {
+            // Notify the server about file changes to '.clientrc files contained in the workspace
+            fileEvents: vscode_1.workspace.createFileSystemWatcher('**/.clientrc')
+        }
+    };
+    // Create the language client and start the client.
+    client = new vscode_languageclient_1.LanguageClient('vscode-hades', 'Hades', serverOptions, clientOptions);
+    // Start the client. This will also launch the server
+    client.start();
+}
+exports.activate = activate;
+function deactivate() {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
+exports.deactivate = deactivate;
