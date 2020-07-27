cc71804d4e368529a9569e03c029e46709fbab37
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2020-07-27 22:25:57 +0530

    tools/generate_c_stubs: Add library functions to dump stack traces

---
+++ tools/generate_c_stubs/lib/Debug.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include <execinfo.h>
+void SG_Debug_dump_int(int value) {
+    fprintf(stderr, "%d", value);
+}
+
+void SG_Debug_dump_string(const char* str) {
+    fprintf(stderr, "%s", str);
+}
+void SG_Debug_dump_newline() {
+    fprintf(stderr, "\n");
+}
+
+
+
+#define MAX_STACK_LEVELS 50
+
+// helper-function to print the current stack trace
+void SG_Debug_dump_stacktrace()
+{
+  void *buffer[MAX_STACK_LEVELS];
+  int levels = backtrace(buffer, MAX_STACK_LEVELS);
+
+  // print to stderr (fd = 2), and remove this function from the trace
+  backtrace_symbols_fd(buffer + 1, levels - 1, 2);
+
+}
