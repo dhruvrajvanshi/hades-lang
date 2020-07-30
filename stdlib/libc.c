1bfe4f83811f0cb14d4ca6d4415de6adaf2f3b72
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2020-07-30 07:46:51 +0530

    stdlib/libc: errno

--- stdlib/libc.c
+++ stdlib/libc.c
@@ -2,6 +2,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <stdio.h>
+#include <errno.h>
 
 const char* hdc_dirent_name(struct dirent* entry) {
     return entry->d_name;
@@ -13,5 +14,12 @@ unsigned char hdc_dirent_type(struct dirent* entry)  {
 
 unsigned char hdc_dirent_is_directory(struct dirent* entry) {
     return S_ISDIR(entry->d_type);
+}
+
+int hdc_get_errno() {
+    return errno;
+}
+void hdc_set_errno(int value) {
+    errno = value;
 }
 
