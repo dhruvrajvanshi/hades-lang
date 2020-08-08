30fdc75b232c42997ed45e24e4eeb0731cebe497
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2020-08-08 16:54:29 +0530

    hades-cpp/ast: Accessors for Param

--- lib/ast/Type.cpp
+++ lib/ast/Type.cpp
@@ -14,13 +14,13 @@ auto Type::location() const -> const SourceLocation & { return m_location; }
 namespace type {
 
 Var::Var(Identifier ident) noexcept
-    : Type(ident.location()), m_name(std::move(ident)) {}
+    : Type(ident.location(), Kind::VAR), m_name(std::move(ident)) {}
 
 Pointer::Pointer(SourceLocation location, const Type *pointee,
                  bool is_mutable) noexcept
-    : Type(location),          //
-      m_pointee(pointee),      //
-      m_is_mutable(is_mutable) //
+    : Type(location, Kind::POINTER), //
+      m_pointee(pointee),            //
+      m_is_mutable(is_mutable)       //
 {}
 
 auto Pointer::pointee() const -> const Type * { return m_pointee; }
