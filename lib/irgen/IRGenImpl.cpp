d3edcb207c153c448bc4d372b8a18cfc3f33eed1
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2020-08-09 15:05:43 +0530

    hades-cpp/IRGenImpl: Skeleton for lower_statement

--- lib/irgen/IRGenImpl.cpp
+++ lib/irgen/IRGenImpl.cpp
@@ -32,19 +32,54 @@ auto t::lower_declaration(const Declaration & decl) -> void {
 }
 
 auto t::lower_extern_def(const ExternDef & def) -> void {
-  assert(def.signature().return_type().hasValue());
-  auto* return_type = lower_type(*def.signature().return_type().getValue());
-  auto param_types = allocator().allocate_array_ref<const Param*, llvm::Type*>(
-      def.signature().params(),
-      [this](const Param* param) -> llvm::Type* {
-        return lower_type(*param->type().getValue());
-      }
-  );
-  const auto* fn_type = llvm::FunctionType::get(return_type, param_types, /* isVarArgs */ false);
+}
+
+auto t::get_extern_def_callee(const ExternDef & def) -> llvm::FunctionCallee * {
   unimplemented();
 }
 
-auto t::lower_function_def(const FunctionDef &) -> void {
+auto t::get_function_signature_type(const FunctionSignature &signature)
+    -> llvm::FunctionType * {
+  assert(signature.return_type().hasValue());
+  auto *return_type = lower_type(*signature.return_type().getValue());
+  auto param_types =
+      allocator().allocate_array_ref<const Param *, llvm::Type *>(
+          signature.params(), [this](const Param *param) -> llvm::Type * {
+            return lower_type(*param->type().getValue());
+          });
+  return llvm::FunctionType::get(return_type, param_types, /* isVarArgs */ false);
+}
+
+auto t::lower_function_def(const FunctionDef & def) -> void {
+  auto* type = get_function_signature_type(def.signature());
+  auto name = def.signature().name().name().as_string_ref();
+  auto* function = llvm::Function::Create(type, llvm::GlobalValue::LinkageTypes::CommonLinkage, 0, name, &llvm_module());
+  auto* basic_block = llvm::BasicBlock::Create(m_llvm_ctx, "entry", function);
+  builder().SetInsertPoint(basic_block);
+
+  for (auto* statement : def.body().statements()) {
+    lower_statement(*statement);
+  }
+}
+
+auto t::lower_statement(const Statement & statement) -> void {
+  switch (statement.kind()) {
+  case Statement::Kind::ERROR: llvm_unreachable("");
+  case Statement::Kind::EXPRESSION:
+    lower_expression_statement(*statement.as<ExpressionStatement>());
+    return;
+  case Statement::Kind::VAL:
+    lower_val_statement(*statement.as<ValStatement>());
+    return;
+  }
+  llvm_unreachable("");
+}
+auto t::lower_expression_statement(const ExpressionStatement &)
+-> void {
+  unimplemented();
+}
+
+auto t::lower_val_statement(const ValStatement & statement) -> void {
   unimplemented();
 }
 
@@ -84,25 +119,25 @@ auto t::lower_type(const Type & type) -> llvm::Type * {
 }
 
 auto t::get_struct_def_type(const StructDef & struct_def) -> llvm::Type * {
-  auto* struct_type = llvm::StructType::create(m_llvm_ctx);
-  struct_type->setName(struct_def.identifier().name().as_string_ref());
-  auto field_types = Vec<llvm::Type*>();
-  for (auto* member : struct_def.members()) {
-    switch (member->kind()) {
-    case StructMember::Kind::FIELD: {
-      auto* field = static_cast<const StructField*>(member);
-      assert(field->type().hasValue());
-      auto* declared_type = field->type().getValue();
-      field_types.push_back(lower_type(*declared_type));
+  if (!m_struct_def_types.contains(&struct_def)) {
+    auto *struct_type = llvm::StructType::create(m_llvm_ctx);
+    struct_type->setName(struct_def.identifier().name().as_string_ref());
+    auto field_types = Vec<llvm::Type *>();
+    for (auto *member : struct_def.members()) {
+      switch (member->kind()) {
+      case StructMember::Kind::FIELD: {
+        auto *field = static_cast<const StructField *>(member);
+        assert(field->type().hasValue());
+        auto *declared_type = field->type().getValue();
+        field_types.push_back(lower_type(*declared_type));
+      }
+      }
     }
-    }
+    struct_type->setBody(allocator().copy_items(field_types));
+    m_struct_def_types.insert({ &struct_def, struct_type });
   }
 
-  struct_type->setBody(allocator().copy_items(field_types));
-
-  struct_type->print(llvm::outs());
-
-  return struct_type;
+  return m_struct_def_types.at(&struct_def);
 }
 
 IRGenImpl::IRGenImpl(core::Context *ctx) noexcept : m_ctx{ctx} {}
