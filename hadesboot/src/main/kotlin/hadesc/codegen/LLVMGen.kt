ea9170c316bb8f617bc0f97902f8a63ee63a675d
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2020-08-21 07:48:19 +0530

    Replace this params in stdlib with self

--- hadesboot/src/main/kotlin/hadesc/codegen/LLVMGen.kt
+++ hadesboot/src/main/kotlin/hadesc/codegen/LLVMGen.kt
@@ -534,6 +534,8 @@ class LLVMGen(private val ctx: Context, private val irModule: IRModule) : AutoCl
             .start()
         val exitCode = process.waitFor()
         assert(exitCode == 0) {
+            log.error(process.inputStream.readAllBytes().toString(StandardCharsets.UTF_8))
+            log.error(process.errorStream.readAllBytes().toString(StandardCharsets.UTF_8))
             log.error("Module: ", LLVM.LLVMPrintModuleToString(llvmModule.ref).string)
             "$cc exited with code $exitCode"
         }
