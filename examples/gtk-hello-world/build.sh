e60832ebf46205d77bccb692781173359c8fb8e0
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2020-08-22 22:45:21 +0530

    examples: Update gtk-hello-world example with new build

--- examples/gtk-hello-world/build.sh
+++
@@ -1,7 +0,0 @@
-
-
-CFLAGS=$(pkg-config --cflags gtk+-3.0)
-LDFLAGS=$(pkg-config --libs gtk+-3.0)
-
-../../gradlew -c ../../settings.gradle :hadesboot:run --args="--main ../examples/gtk-hello-world/main.hds --directories ../stdlib ../bindings --runtime ../runtime.c --output ../examples/app --cflags ../examples/gtk-hello-world/main.c $CFLAGS $LDFLAGS"
-
