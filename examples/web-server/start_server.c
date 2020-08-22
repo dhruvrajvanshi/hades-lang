a4f645b1344fc9201973ccfe3de239209d7cc96c
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2020-08-22 19:24:08 +0530

    examples/web-server: bind socket

---
+++ examples/web-server/start_server.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define PORT    6666
+#define MAXMSG  512
+
+int bind_socket(uint16_t port) {
+  int sock;
+  struct sockaddr_in name;
+
+  /* Create the socket. */
+  sock = socket (PF_INET, SOCK_STREAM, 0);
+  if (sock < 0) {
+    perror ("socket");
+    exit (EXIT_FAILURE);
+  }
+
+  /* Give the socket a name. */
+  name.sin_family = AF_INET;
+  name.sin_port = htons (port);
+  name.sin_addr.s_addr = htonl (INADDR_ANY);
+  if (bind (sock, (struct sockaddr *) &name, sizeof (name)) < 0) {
+    perror ("bind");
+    exit (EXIT_FAILURE);
+  }
+
+  return sock;
+}
+
+extern int handle_request(int fd);
+
+int start_server () {
+  int sock;
+  fd_set active_fd_set, read_fd_set;
+  int i;
+  struct sockaddr_in clientname;
+  size_t size;
+
+  /* Create the socket and set it up to accept connections. */
+  perror("creating socket");
+  sock = make_socket (PORT);
+  perror("socket created");
+  if (listen (sock, 1) < 0)
+    {
+      perror ("listen");
+      exit (EXIT_FAILURE);
+    }
+
+  /* Initialize the set of active sockets. */
+  FD_ZERO (&active_fd_set);
+  FD_SET (sock, &active_fd_set);
+
+  while (1)
+    {
+      /* Block until input arrives on one or more active sockets. */
+      read_fd_set = active_fd_set;
+      fprintf(stderr, "in loop\n");
+      if (select (FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0)
+        {
+          perror ("select");
+          exit (EXIT_FAILURE);
+        }
+      fprintf(stderr, "selected\n");
+
+      /* Service all the sockets with input pending. */
+      for (i = 0; i < FD_SETSIZE; ++i)
+        if (FD_ISSET (i, &read_fd_set))
+          {
+            fprintf(stderr, "fd set\n");
+            if (i == sock)
+              {
+                /* Connection request on original socket. */
+                int new;
+                size = sizeof (clientname);
+                new = accept (sock,
+                              (struct sockaddr *) &clientname,
+                              &size);
+                fprintf(stderr, "accepted\n");
+                if (new < 0)
+                  {
+                    perror ("accept");
+                    exit (EXIT_FAILURE);
+                  }
+                fprintf (stderr,
+                         "Server: connect.\n");
+                fprintf(stderr, "calling FD_SET\n");
+                FD_SET (new, &active_fd_set);
+                perror("FD_SET returned");
+              }
+            else
+              {
+                /* Data arriving on an already-connected socket. */
+                perror("data arriving");
+                if (handle_request(i) < 0)
+                  {
+                    close (i);
+                    FD_CLR (i, &active_fd_set);
+                  }
+              }
+          }
+    }
+}
+
