7a3d0370ae74d26096f96f39e7847e165d3fba11
Dhruv Rajvanshi <dhruv.rajvanshi@blackrock.com>
2020-12-30 08:41:08 -0500

    hscript: Add debug helpers

--- hscript/src/hscript/lib/debug.c
+++ hscript/src/hscript/lib/debug.c
@@ -1,9 +1,13 @@
 #include "stdint.h"
 #include "stdio.h"
 
-void hscript_lib_dump_size(FILE* file, const char* tag, size_t size) {
-    fprintf(file, "%s: %lu\n", tag, size);
+void hscript_lib_dump_size(FILE* file, size_t size) {
+    fprintf(file, "%lu", size);
 }
-void hscript_lib_dump_ptr(FILE* file, const char* tag, void* ptr) {
-    fprintf(file, "%s: %x\n", tag, ptr);
+void hscript_lib_dump_ptr(FILE* file, void* ptr) {
+    fprintf(file, "%x", ptr);
+}
+
+void hscript_lib_dump_f64(FILE* file, double value) {
+    fprintf(file, "%f", value);
 }
