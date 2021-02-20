cdd50f176ae66a1147f4691639ecbf5ea493cfd4
Dhruv <dhruvrajvanshi@outlook.com>
2021-02-20 15:55:27 +0530

    hadesboot: Clean up test runner

--- hadesboot/src/test/kotlin/hades/test/HadesTestSuite.kt
+++ hadesboot/src/test/kotlin/hades/test/HadesTestSuite.kt
@@ -3,10 +3,7 @@ package hades.test
 import hadesc.Compiler
 import hadesc.logging.logger
 import org.apache.commons.lang3.SystemUtils
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.DynamicNode
-import org.junit.jupiter.api.DynamicTest
-import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.*
 import java.io.File
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -15,7 +12,6 @@ import kotlin.test.assertEquals
 
 class HadesTestSuite {
 
-    @OptIn(ExperimentalStdlibApi::class)
     @TestFactory
     fun `should run test suite`(): List<DynamicNode> {
         val directory = File("test")
@@ -28,94 +24,95 @@ class HadesTestSuite {
         }
         outputDirectory.mkdirs()
         val files = directory.listFiles() ?: arrayOf()
-        return buildList {
-            for (file in files.sortedBy { it.name }) {
-                if (file.extension == "hds") {
 
-                    val expectedStdoutFile = Paths.get(
-                            directory.toPath().toString(),
-                            file.nameWithoutExtension + ".stdout"
-                    ).toFile()
-                    val expectedErrorsFile = Paths.get(
-                            directory.toPath().toString(),
-                            file.nameWithoutExtension + ".errors"
-                    ).toFile()
-                    val cSourceFile = Paths.get(
-                            directory.toPath().toString(),
-                            file.nameWithoutExtension + ".c"
-                    ).toFile()
-                    add(DynamicTest.dynamicTest(file.name) {
-                        logger().debug("Running suite file {}", file)
+        return files
+            .sortedBy { it.name }
+            .filter { it.extension == "hds" }
+            .map { file -> DynamicTest.dynamicTest(file.nameWithoutExtension, file.toURI()) {
+                val expectedStdoutFile = Paths.get(
+                    directory.toPath().toString(),
+                    file.nameWithoutExtension + ".stdout"
+                ).toFile()
+                val expectedErrorsFile = Paths.get(
+                    directory.toPath().toString(),
+                    file.nameWithoutExtension + ".errors"
+                ).toFile()
+                val cSourceFile = Paths.get(
+                    directory.toPath().toString(),
+                    file.nameWithoutExtension + ".c"
+                ).toFile()
 
-                        val outputPath = Paths.get(
-                            outputDirectory.toString(),
-                            if (SystemUtils.IS_OS_WINDOWS)
-                                file.nameWithoutExtension + ".exe"
-                            else
-                                file.nameWithoutExtension
-                        )
-                        val cSources = if (cSourceFile.exists()) {
-                            cSourceFile.toString()
-                        } else {
-                            ""
-                        }
-                        val compiler = Compiler(
-                            arrayOf(
-                                "--output", outputPath.toString(),
-                                "--directories", "stdlib", directory.toString(),
-                                "--main", file.toString(),
-                                "--runtime", "runtime.c",
-                                "--dump-llvm-module",
-                                "--c-sources", "stdlib/libc.c", cSources,
-                                "--cflags", utilsCLib.toString(), "-D", "DEBUG"
-                            )
-                            .filter { it.isNotBlank() }
-                            .toTypedArray()
-                        )
-                        val diagnostics = compiler.run()
-                        if (expectedStdoutFile.exists()) {
-                            assert(File(outputPath.toUri()).exists()) {
-                                "Expected $outputPath to be present after compilation"
-                            }
-                            val actualStdoutFile =
-                                Path.of(outputDirectory.toString(), file.nameWithoutExtension + ".stdout")
-                                    .toFile()
+                logger().debug("Running suite file {}", file)
 
-                            val process = ProcessBuilder(outputPath.toString())
-                                .redirectError(ProcessBuilder.Redirect.INHERIT)
-                                .redirectOutput(actualStdoutFile)
-                                .start()
-                            val hasExited = process.waitFor(1, TimeUnit.SECONDS)
-                            if (!hasExited) {
-                                process.destroy()
-                            }
-                            assert(hasExited)
-                            print(actualStdoutFile.readText())
-                            assert(process.exitValue() == 0) {
-                                "Test program exited with non-zero exit code"
-                            }
-                            val expectedLines = expectedStdoutFile.readLines()
-                            val actualLines = actualStdoutFile.readLines()
-                            assertEquals(
-                                expectedLines, actualLines,
-                                "Contents of $expectedStdoutFile and $actualStdoutFile don't match"
-                            )
-                        } else {
-                            assert(expectedErrorsFile.exists())
-                            val expectedErrors = expectedErrorsFile.readLines().joinToString("\n")
-                            val actualErrors = diagnostics
-                                    .sortedBy { it.sourceLocation.start }
-                                    .joinToString("\n") {
-                                        "${it.sourceLocation.file.path.toString().replace('\\', '/')}:${it.sourceLocation.start.line}: ${it.kind::class.simpleName}"
-                                    }
-                            Assertions.assertEquals(
-                                    expectedErrors,
-                                    actualErrors
-                            )
-                        }
-
-                    })
+                val outputPath = Paths.get(
+                    outputDirectory.toString(),
+                    if (SystemUtils.IS_OS_WINDOWS)
+                        file.nameWithoutExtension + ".exe"
+                    else
+                        file.nameWithoutExtension
+                )
+                val cSources = if (cSourceFile.exists()) {
+                    cSourceFile.toString()
+                } else {
+                    ""
                 }
+                val compiler = Compiler(
+                    arrayOf(
+                        "--output", outputPath.toString(),
+                        "--directories", "stdlib", directory.toString(),
+                        "--main", file.toString(),
+                        "--runtime", "runtime.c",
+                        "--dump-llvm-module",
+                        "--c-sources", "stdlib/libc.c", cSources,
+                        "--cflags", utilsCLib.toString(), "-D", "DEBUG"
+                    )
+                        .filter { it.isNotBlank() }
+                        .toTypedArray()
+                )
+                val diagnostics = compiler.run()
+                if (expectedStdoutFile.exists()) {
+                    assert(File(outputPath.toUri()).exists()) {
+                        "Expected $outputPath to be present after compilation"
+                    }
+                    val actualStdoutFile =
+                        Path.of(outputDirectory.toString(), file.nameWithoutExtension + ".stdout")
+                            .toFile()
+
+                    val process = ProcessBuilder(outputPath.toString())
+                        .redirectError(ProcessBuilder.Redirect.INHERIT)
+                        .redirectOutput(actualStdoutFile)
+                        .start()
+                    val hasExited = process.waitFor(1, TimeUnit.SECONDS)
+                    if (!hasExited) {
+                        process.destroy()
+                    }
+                    assert(hasExited)
+                    print(actualStdoutFile.readText())
+                    assert(process.exitValue() == 0) {
+                        "Test program exited with non-zero exit code"
+                    }
+                    val expectedLines = expectedStdoutFile.readLines()
+                    val actualLines = actualStdoutFile.readLines()
+                    assertEquals(
+                        expectedLines, actualLines,
+                        "Contents of $expectedStdoutFile and $actualStdoutFile don't match"
+                    )
+                } else {
+                    assert(expectedErrorsFile.exists())
+                    val expectedErrors = expectedErrorsFile.readLines().joinToString("\n")
+                    val actualErrors = diagnostics
+                        .sortedBy { it.sourceLocation.start }
+                        .joinToString("\n") {
+                            "${
+                                it.sourceLocation.file.path.toString().replace('\\', '/')
+                            }:${it.sourceLocation.start.line}: ${it.kind::class.simpleName}"
+                        }
+                    Assertions.assertEquals(
+                        expectedErrors,
+                        actualErrors
+                    )
+                }
+
             }
         }
     }
