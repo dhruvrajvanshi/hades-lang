ece4349c798804a47139eadab45e278aedebbd73
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2021-08-01 20:10:04 +0530

    Fix main function for new cli parser

--- hadesboot/src/main/kotlin/hadesc/Main.kt
+++ hadesboot/src/main/kotlin/hadesc/Main.kt
@@ -1,7 +1,13 @@
 package hadesc
 
+import kotlin.system.exitProcess
+
 fun main(args: Array<String>) {
     val compiler = HadesCompiler()
     compiler.main(args)
-    compiler.run()
+    val diagnostics = compiler.execute()
+    if (!diagnostics.isEmpty()) {
+        System.err.println("Compilation errors detected")
+        exitProcess(1)
+    }
 }
