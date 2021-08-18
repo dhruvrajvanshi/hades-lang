81ac575abf70b963e21c4f1c7ea6543cda9ad4da
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2021-08-18 17:54:48 +0530

    Skip test for distributions

--- build_all_distributions.sh
+++ build_all_distributions.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-./gradlew distTar
-./gradlew linux-x86_64DistTar
-./gradlew macos-x86_64DistTar
-./gradlew windows-x86_64DistTar
+./gradlew -x test distTar
+./gradlew -x test linux-x86_64DistTar
+./gradlew -x test macos-x86_64DistTar
+./gradlew -x test windows-x86_64DistTar
