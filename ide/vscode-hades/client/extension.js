73101ab8fca975a12e30f86cb50c8a4fc2656ae0
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2021-11-14 16:07:06 +0530

    ide/vscode: Remove language server

--- ide/vscode-hades/client/extension.js
+++ ide/vscode-hades/client/extension.js
@@ -5,31 +5,6 @@ var vscode_1 = require("vscode");
 var vscode_languageclient_1 = require("vscode-languageclient");
 var client;
 function activate(context) {
-    // If the extension is launched in debug mode then the debug server options are used
-    // Otherwise the run options are used
-    var debugOpts = "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005";
-    var serverOptions = {
-        command: "/home/dhruv/Projects/hades-lang/language-server/target/pack/bin/hades-language-server",
-        options: {
-            env: {
-                // "JAVA_OPTS": debugOpts,
-                "JAVA_HOME": "/home/dhruv/.jdks/adopt-openjdk-11.0.8"
-            }
-        }
-    };
-    // Options to control the language client
-    var clientOptions = {
-        // Register the server for plain text documents
-        documentSelector: [{ scheme: 'file', language: 'hades' }],
-        synchronize: {
-            // Notify the server about file changes to '.clientrc files contained in the workspace
-            fileEvents: vscode_1.workspace.createFileSystemWatcher('**/.clientrc')
-        }
-    };
-    // Create the language client and start the client.
-    client = new vscode_languageclient_1.LanguageClient('vscode-hades', 'Hades', serverOptions, clientOptions);
-    // Start the client. This will also launch the server
-    client.start();
 }
 exports.activate = activate;
 function deactivate() {
--- ide/vscode-hades/client/extension.ts
+++ ide/vscode-hades/client/extension.ts
@@ -9,41 +9,6 @@ import {
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-
-  // If the extension is launched in debug mode then the debug server options are used
-  // Otherwise the run options are used
-
-  let debugOpts = "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
-  let serverOptions: ServerOptions = {
-      command: `/home/dhruv/Projects/hades-lang/language-server/target/pack/bin/hades-language-server`,
-      options: {
-        env: {
-          "JAVA_OPTS": debugOpts,
-          "JAVA_HOME": "/home/dhruv/.jdks/adopt-openjdk-11.0.8"
-        }
-      }
-  };
-
-  // Options to control the language client
-  let clientOptions: LanguageClientOptions = {
-    // Register the server for plain text documents
-    documentSelector: [{ scheme: 'file', language: 'hades' }],
-    synchronize: {
-      // Notify the server about file changes to '.clientrc files contained in the workspace
-      fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-    }
-  };
-
-  // Create the language client and start the client.
-  client = new LanguageClient(
-    'vscode-hades',
-    'Hades',
-    serverOptions,
-    clientOptions
-  );
-
-  // Start the client. This will also launch the server
-  client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
