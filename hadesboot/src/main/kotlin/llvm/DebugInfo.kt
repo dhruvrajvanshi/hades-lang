a6932d8b2e7c75b26ed785aa5f44c97157d3b2b9
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2022-02-04 18:21:41 +0530

    Add createCompileUnit method for DebugInfoBuilder

--- hadesboot/src/main/kotlin/llvm/DebugInfo.kt
+++ hadesboot/src/main/kotlin/llvm/DebugInfo.kt
@@ -3,6 +3,49 @@ package llvm
 import org.bytedeco.llvm.LLVM.*
 import org.bytedeco.llvm.global.LLVM
 
+fun LLVMDIBuilderRef.createCompileUnit(
+    fileRef: LLVMMetadataRef
+): LLVMMetadataRef =
+    LLVM.LLVMDIBuilderCreateCompileUnit(
+        // LLVMDIBuilderRef Builder
+        this,
+        // @Cast("LLVMDWARFSourceLanguage") int Lang
+        LLVM.LLVMDWARFSourceLanguageC,
+        // LLVMMetadataRef FileRef,
+        fileRef,
+        // @Cast("const char*") BytePointer Producer,
+        "hadesboot",
+        // @Cast("size_t") long ProducerLen,
+        "hadesboot".length.toLong(),
+        // @Cast("LLVMBool") int isOptimized
+        false.toLLVMBool(),
+        // @Cast("const char*") BytePointer Flags
+        null,
+        // @Cast("size_t") long FlagsLen,
+        0,
+        // @Cast("unsigned") int RuntimeVer,
+        1,
+        // @Cast("const char*") BytePointer SplitName,
+        null,
+        // @Cast("size_t") long SplitNameLen
+        0,
+        // @Cast("LLVMDWARFEmissionKind") int Kind
+        LLVM.LLVMDWARFEmissionFull,
+        // @Cast("unsigned") int DWOId
+        0,
+        // @Cast("LLVMBool") int SplitDebugInlining
+        false.toLLVMBool(),
+        // @Cast("LLVMBool") int DebugInfoForProfiling
+        false.toLLVMBool(),
+        // @Cast("const char*") BytePointer SysRoot,
+        null,
+        // @Cast("size_t") long SysRootLen,
+        0,
+        // @Cast("const char*") BytePointer SDK
+        null,
+        // @Cast("size_t") long SDKLen
+        0
+    )
 @Suppress("unused")
 fun LLVMDIBuilderRef.createLexicalBlock(scope: LLVMMetadataRef, file: LLVMMetadataRef, line: Int, column: Int): LLVMMetadataRef =
     LLVM.LLVMDIBuilderCreateLexicalBlock(this, scope, file, line, column)
