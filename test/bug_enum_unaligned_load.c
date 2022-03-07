fa03523ad25a82ceffdd986dc6676f3bd289c44b
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2022-03-07 19:46:37 +0530

    Fix: Fix size_t format specifier to a portable one

--- test/bug_enum_unaligned_load.c
+++ test/bug_enum_unaligned_load.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#if __APPLE__
-    #define SIZE_T_FORMAT "%zu"
-#else
-    #define SIZE_T_FORMAT "%llu"
-#endif
 void hscript_put_usize(size_t size, FILE* file) {
-    fprintf(file, SIZE_T_FORMAT, size);
+    fprintf(file, "%zu", size);
 }
