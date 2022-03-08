8a7113ffdad69bf024ef48ea4ec883a6cb7ec478
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2022-03-08 14:04:34 +0530

    Refactor: Use HIRBuilder methods in DesugarWhenExpressions.kt

--- hadesboot/src/main/kotlin/hadesc/hir/passes/DesugarWhenExpressions.kt
+++ hadesboot/src/main/kotlin/hadesc/hir/passes/DesugarWhenExpressions.kt
@@ -2,11 +2,7 @@ package hadesc.hir.passes
 
 import hadesc.context.GlobalConstantContext
 import hadesc.context.NamingContext
-import hadesc.hir.HIRBlock
-import hadesc.hir.HIRConstant
-import hadesc.hir.HIRExpression
-import hadesc.hir.HIRStatement
-import hadesc.hir.BinaryOperator
+import hadesc.hir.*
 import hadesc.types.Type
 
 class DesugarWhenExpressions(
@@ -14,49 +10,13 @@ class DesugarWhenExpressions(
     private val constCtx: GlobalConstantContext
 ) : AbstractHIRTransformer() {
     override fun transformWhenExpression(expression: HIRExpression.When): HIRExpression {
-        val blockStatements = requireNotNull(currentStatements)
-        val discriminantName = namingCtx.makeUniqueName()
-        val resultName = namingCtx.makeUniqueName()
-        blockStatements.addAll(
-            listOfNotNull(
-                HIRStatement.ValDeclaration(
-                    expression.discriminant.location,
-                    discriminantName,
-                    isMutable = false,
-                    expression.discriminant.type
-                ),
-                HIRStatement.ValDeclaration(
-                    expression.location,
-                    resultName,
-                    isMutable = true,
-                    expression.type
-                ),
-                HIRStatement.Assignment(
-                    expression.discriminant.location,
-                    discriminantName,
-                    transformExpression(expression.discriminant)
-                )
-            )
-        )
+        val discriminantRef = declareAndAssign("match_discriminant", transformExpression(expression.discriminant))
+        val resultRef = declareVariable("match_result", expression.type)
 
-        val discriminant = HIRExpression.ValRef(
-            expression.location,
-            expression.discriminant.type,
-            discriminantName
-        )
-        val discriminantTag = HIRExpression.GetStructField(
-            expression.location,
-            constCtx.enumTagType,
-            lhs = discriminant,
-            name = namingCtx.makeName("\$tag"),
-            index = 0
-        )
+        val discriminantTag = discriminantRef.getStructField("\$tag", 0, constCtx.enumTagType)
 
-        val discriminantPtr = HIRExpression.AddressOf(
-            expression.discriminant.location,
-            Type.Ptr(discriminant.type, isMutable = false),
-            discriminantName
-        )
+        val discriminantPtr = addressOf(discriminantRef)
+
         expression.cases.forEachIndexed { index, case ->
             val trueBranch = HIRBlock(
                 case.expression.location,
@@ -82,12 +42,12 @@ class DesugarWhenExpressions(
                     ),
                     HIRStatement.Assignment(
                         case.expression.location,
-                        resultName,
+                        resultRef.name,
                         transformExpression(case.expression)
                     )
                 )
             )
-            blockStatements.add(
+            emit(
                 HIRStatement.ifStatement(
                     case.expression.location,
                     HIRExpression.BinOp(
@@ -106,10 +66,6 @@ class DesugarWhenExpressions(
                 )
             )
         }
-        return HIRExpression.ValRef(
-            expression.location,
-            expression.type,
-            resultName
-        )
+        return resultRef
     }
 }
