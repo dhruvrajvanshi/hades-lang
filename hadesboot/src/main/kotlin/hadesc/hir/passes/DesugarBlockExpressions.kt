3e7e70c8c29e0a135f4edba8fa4e546cebef7d0a
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2022-03-27 02:29:25 +0530

    [refactor]: Simplify pass

--- hadesboot/src/main/kotlin/hadesc/hir/passes/DesugarBlockExpressions.kt
+++ hadesboot/src/main/kotlin/hadesc/hir/passes/DesugarBlockExpressions.kt
@@ -2,37 +2,27 @@ package hadesc.hir.passes
 
 import hadesc.context.NamingContext
 import hadesc.hir.*
-import hadesc.types.Type
 
 class DesugarBlockExpressions(override val namingCtx: NamingContext): AbstractHIRTransformer() {
 
     override fun transformBlockExpression(expression: HIRExpression.BlockExpression): HIRExpression {
         val resultName = namingCtx.makeUniqueName()
         emitAlloca(resultName, expression.type)
-        intoStatementList(checkNotNull(currentStatements)) {
-            val initialBlock = transformBlock(expression.block)
-            val block = when (val lastMember = initialBlock.statements.lastOrNull()) {
-                is HIRStatement.Expression -> {
-                    val blockStatements = mutableListOf<HIRStatement>()
-                    intoStatementList(blockStatements) {
-                        emitAll(initialBlock.statements.dropLast(1))
-                        emit(HIRStatement.Assignment(
-                            lastMember.location,
-                            resultName,
-                            transformExpression(lastMember.expression)
-                        ))
-                    }
-
-                    HIRBlock(
-                        initialBlock.location,
-                        namingCtx.makeUniqueName(),
-                        blockStatements
-                    )
+        val initialBlock = transformBlock(expression.block)
+        val block = when (val lastMember = initialBlock.statements.lastOrNull()) {
+            is HIRStatement.Expression -> {
+                buildBlock {
+                    emitAll(initialBlock.statements.dropLast(1))
+                    emit(HIRStatement.Assignment(
+                        lastMember.location,
+                        resultName,
+                        transformExpression(lastMember.expression)
+                    ))
                 }
-                else -> initialBlock
             }
-            emitAll(block.statements)
+            else -> initialBlock
         }
+        emitAll(block.statements)
 
         return HIRExpression.ValRef(
             expression.location,
