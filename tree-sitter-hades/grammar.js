ed20ff44a0c5b474775cc3bc2ebd8d217d67e330
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2022-04-09 12:11:02 +0530

    Started work on tree-sitter

---
+++ tree-sitter-hades/grammar.js
@@ -0,0 +1,22 @@
+module.exports = grammar({
+  name: 'Hades',
+  rules: {
+    source_file: $ => repeat($.definition),
+    definition: $ => choice(
+      $.def_function,
+    ),
+    identifier: $ => /[a-zA-Z_][a-zA-Z0-9_]/,
+    def_function: $ => seq(
+      'def',
+      $.identifier,
+      '(',
+      ')',
+      ':',
+      $.type,
+    ),
+    type: $ => choice(
+      $.type_var
+    ),
+    type_var: $ => $.identifier
+  }
+})
