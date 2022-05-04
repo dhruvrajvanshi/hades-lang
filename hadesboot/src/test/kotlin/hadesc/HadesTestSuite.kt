e49df50d9ed4aa3d592240c17b976b8920ba695c
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2022-05-04 14:07:23 +0530

    Revert "Fix metadata cast for debug symbols"
    This reverts commit 3f5a46c131c7a127988503e4c17737846ddf55fe.
    This was an accidental commit

--- hadesboot/src/test/kotlin/hadesc/HadesTestSuite.kt
+++ hadesboot/src/test/kotlin/hadesc/HadesTestSuite.kt
@@ -56,7 +56,6 @@ class HadesTestSuite {
                     "--module-path", directory.toString(),
                     "--main", file.toString(),
                     "--enable-hir-verifier",
-                    "--debug-symbols",
                 ).apply {
                     add("--c-source")
                     add(utilsCLib.toString())
