d0852b4070d8e24614a26fa5e1a9ae9cb38558b2
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2022-08-01 16:17:40 +0200

    Revert "Enable k2 compiler"
    This reverts commit 5ecfab18add5acd9dfb92b7f06b3570a997e1f18.

--- hadesboot/build.gradle.kts
+++ hadesboot/build.gradle.kts
@@ -50,7 +50,7 @@ tasks.test {
 tasks.compileKotlin {
     kotlinOptions.jvmTarget = "15"
     kotlinOptions {
-        freeCompilerArgs = listOf("-Xinline-classes", "-Xuse-k2")
+        freeCompilerArgs = listOf("-Xinline-classes")
     }
 }
 tasks.compileTestKotlin {
