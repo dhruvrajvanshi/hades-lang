4c870062ac618b329325dfa93627eaaf8e1f546c
Dhruv Rajvanshi <d@rajv.dev>
2022-10-07 17:47:13 +0200

    Allow passing of some flags using hades.yml
    Super basic. If schema doesn't match, it just throws an exception

--- hadesboot/src/main/kotlin/hadesc/BuildOptions.kt
+++ hadesboot/src/main/kotlin/hadesc/BuildOptions.kt
@@ -1,8 +1,23 @@
 package hadesc
 
+import kotlinx.serialization.Serializable
 import java.nio.file.Path
 
 sealed interface Options
+
+@Serializable
+data class AllOptions(
+    val build: YamlBuildOptions
+)
+
+@Serializable
+data class YamlBuildOptions(
+    val cSources: List<String> = emptyList(),
+    val cFlags: List<String> = emptyList(),
+    val libs: List<String> = emptyList(),
+    val directories: List<String> = emptyList(),
+)
+
 data class BuildOptions(
     val directories: List<Path>,
     val output: Path,
