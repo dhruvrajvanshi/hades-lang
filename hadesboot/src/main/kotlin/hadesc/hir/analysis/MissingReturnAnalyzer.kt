d4287bdc086c50ee84a3fa227ed2476ad214f656
Dhruv Rajvanshi <d@rajv.dev>
2022-10-16 15:37:37 +0200

    Show diagnostic on missing return
    Fixes #30

---
+++ hadesboot/src/main/kotlin/hadesc/hir/analysis/MissingReturnAnalyzer.kt
@@ -0,0 +1,19 @@
+package hadesc.hir.analysis
+
+import hadesc.context.Context
+import hadesc.diagnostics.Diagnostic
+import hadesc.hir.ControlFlowVisitor
+import hadesc.hir.HIRBlock
+
+class MissingReturnAnalyzer(private val ctx: Context): ControlFlowVisitor() {
+    override fun visitBlock(block: HIRBlock) {
+        super.visitBlock(block)
+        if (block.statements.isEmpty()) {
+            ctx.diagnosticReporter.report(
+                block.location,
+                Diagnostic.Kind.MissingReturnValue
+            )
+        }
+    }
+
+}
