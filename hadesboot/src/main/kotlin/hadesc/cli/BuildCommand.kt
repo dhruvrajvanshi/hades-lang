d537494659087a0bb4362b0e715aad6384fae882
Dhruv Rajvanshi <d@rajv.dev>
2022-10-16 11:01:01 +0200

    Fix double build in test runner

--- hadesboot/src/main/kotlin/hadesc/cli/BuildCommand.kt
+++ hadesboot/src/main/kotlin/hadesc/cli/BuildCommand.kt
@@ -2,6 +2,8 @@ package hadesc.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.groups.provideDelegate
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.path
@@ -9,7 +11,6 @@ import hadesc.context.BuildTarget
 import hadesc.BuildCLIOptions
 import hadesc.context.Context
 import hadesc.diagnostics.Diagnostic
-import hadesc.ignore
 import hadesc.logging.logger
 
 class BuildCommand: CliktCommand(invokeWithoutSubcommand = true) {
@@ -17,8 +18,18 @@ class BuildCommand: CliktCommand(invokeWithoutSubcommand = true) {
     private val buildOptions by BuildCLIOptions()
     private val output by option("--output", "-o").path().required()
     private val main by option("--main").path().required()
+    private val skipExec by option("--internal-skip-exec")
+        // we have this flag because HadesTestSuite
+        // needs to call execute manually to get the list of diagnostics
+        // as a List
+        .help("Internal flag. To be used only by the implementation").flag()
 
-    override fun run() = execute().ignore()
+    override fun run() {
+        if (skipExec) {
+            return
+        }
+        execute()
+    }
 
     fun execute(): List<Diagnostic> {
         val options = buildOptions.toBuildOptions()
