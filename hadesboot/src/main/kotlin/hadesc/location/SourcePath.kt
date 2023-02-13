42af822faa36fc6bbe270084c562aac10b073d4a
Dhruv Rajvanshi <d@rajv.dev>
2023-02-13 11:01:41 +0100

    Allow emitting JSON diagnostics

--- hadesboot/src/main/kotlin/hadesc/location/SourcePath.kt
+++ hadesboot/src/main/kotlin/hadesc/location/SourcePath.kt
@@ -1,14 +1,31 @@
 package hadesc.location
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.net.URI
 import java.nio.file.Path
 
 
-@Serializable
+@Serializable(with = SourcePathToURISerializer::class)
 data class SourcePath(
     val path: Path
 ) {
     override fun toString(): String {
         return path.toString()
     }
+}
+
+private class SourcePathToURISerializer: KSerializer<SourcePath> {
+    override val descriptor = PrimitiveSerialDescriptor("SourcePath", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): SourcePath =
+        SourcePath(Path.of(URI.create(decoder.decodeString())))
+
+    override fun serialize(encoder: Encoder, value: SourcePath) =
+        encoder.encodeString(value.path.toUri().toString())
+
 }
