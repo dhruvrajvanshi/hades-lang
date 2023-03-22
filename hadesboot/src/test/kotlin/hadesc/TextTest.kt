87c813a97801ec7e7cd3f2d43f25952ebeb22859
Dhruv Rajvanshi <dhruv.rajvanshi@scalable.capital@Dhruvs-Laptop.local>
2023-03-22 19:05:18 +0100

    Implement text indexing

--- hadesboot/src/test/kotlin/hadesc/TextTest.kt
+++ hadesboot/src/test/kotlin/hadesc/TextTest.kt
@@ -1,10 +1,18 @@
 package hadesc
 
+import hadesc.text.Config
 import hadesc.text.Text
+import org.junit.jupiter.api.BeforeEach
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class TextTest {
+    @BeforeEach
+    fun beforeEach() {
+        Config.branchingFactor = 3
+        Config.maxChunkSize = 3
+    }
+
     @Test
     fun `offsetOf for string with one line`() {
         val str = Text.from("foo")
@@ -17,5 +25,15 @@ class TextTest {
         val str = Text.from("foo\nbar\nbax")
         assertEquals(4, str.offsetOf(2, 1))
         assertEquals(5, str.offsetOf(2, 2))
+    }
+
+    @Test
+    fun `indexing into Interior nodes works correctly`() {
+        val str = Text.from("012345678901234567890")
+        assertEquals('0', str[0])
+        assertEquals('1', str[1])
+        assertEquals('2', str[2])
+        assertEquals('0', str[10])
+        assertEquals('1', str[11])
     }
 }
