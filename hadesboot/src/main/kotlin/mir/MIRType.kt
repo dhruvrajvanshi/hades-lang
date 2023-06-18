68c7ad5057ada84c0767fa5e0281e9f9aa995165
Dhruv Rajvanshi <dhruv.rajvanshi@scalable.capital@Dhruvs-Laptop.local>
2023-06-18 07:19:38 +0200

    mir: Add interface type

--- hadesboot/src/main/kotlin/mir/MIRType.kt
+++ hadesboot/src/main/kotlin/mir/MIRType.kt
@@ -6,4 +6,8 @@ sealed interface MIRType {
         val paramTypes: List<MIRType>,
         val returnType: MIRType,
     ): MIRType
+    data class Interface(
+        val types: Map<String, MIRType>,
+        val values: Map<String, MIRType>,
+    )
 }
