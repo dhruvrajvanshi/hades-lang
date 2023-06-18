144560e36f877db201cf421cb29fb7e3c58cfc4c
Dhruv Rajvanshi <dhruv.rajvanshi@scalable.capital@Dhruvs-Laptop.local>
2023-06-18 11:09:02 +0530

    mir: Add MIRVisitor

--- hadesboot/src/main/kotlin/mir/visitor.kt
+++ hadesboot/src/main/kotlin/mir/visitor.kt
@@ -1,1 +1,28 @@
 package mir
+
+interface MIRVisitor {
+    fun visitObject(module: MIRValue.Object) {
+        for ((_, value) in module.values.entries) {
+            visitValue(value)
+        }
+    }
+    fun visitValue(value: MIRValue) = when(value) {
+        is MIRValue.Function -> {
+            for (basicBlock in value.basicBlocks) {
+                visitBasicBlock(basicBlock)
+            }
+        }
+        is MIRValue.I32 -> Unit
+        is MIRValue.Object -> visitObject(value)
+    }
+
+    fun visitBasicBlock(basicBlock: MIRBasicBlock) {
+        for (instruction in basicBlock.instructions) {
+            visitInstruction(instruction)
+        }
+    }
+
+    fun visitInstruction(instruction: MIRInstruction): Unit = when(instruction) {
+        is MIRInstruction.Return -> visitValue(instruction.value)
+    }
+}
