3524b207cc65565f14bd54acbca2513f47059df3
Dhruv Rajvanshi <d@rajv.dev>
2024-06-01 09:35:41 +0200

    Add Module type

--- hadesboot-middle/src/main/kotlin/hadesboot/middle/IR.kt
+++ hadesboot-middle/src/main/kotlin/hadesboot/middle/IR.kt
@@ -1,16 +1,31 @@
 package hadesboot.middle
 
+data class Module(
+    val name: String,
+    val items: List<Item>,
+)
+
+sealed interface Item
+
 data class Fn(
     val name: String,
     val returnType: Type,
     val parameters: List<Parameter>,
     val body: Block
-)
+): Item
+
+data class ExternFn(
+    val name: String,
+    val returnType: Type,
+    val parameters: List<Type>,
+): Item
 
 data class Block(
     val label: String,
-    val instructions: List<Instruction>
+    val instructions: List<Instruction>,
+    val terminator: Terminator,
 )
 
 sealed interface Instruction
+sealed interface Terminator
 data class Parameter(val name: String, val type: Type)
